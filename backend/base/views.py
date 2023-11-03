55274baa5f30b88dc260e547ee806329b7618425
psn0w <08yaksh08@gmail.com>
2023-11-04 03:38:06 +0530

    initial route created

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['GET'])
+def get_routes(request):
+    """View to display all the endpoints"""
+    routes = {
+        "none": "no routes created yet"
+    }
+    return Response(routes)
